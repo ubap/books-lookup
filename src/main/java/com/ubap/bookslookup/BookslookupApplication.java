053349b1e0b40a14c474b569dd01f83b53171e43
Jakub Trzebiatowski <jaktrzeb@gmail.com>
2018-10-25 10:04:10 +0200

    Initial commit

---
+++ src/main/java/com/ubap/bookslookup/BookslookupApplication.java
@@ -0,0 +1,12 @@
+package com.ubap.bookslookup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookslookupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookslookupApplication.class, args);
+    }
+}
